c0187e0963a148bc370ad9aa186c97f9ccd8b0b5
Eric Cohen <eirc.eirc@gmail.com>
2014-04-04 23:56:50 +0300

    Ensure we always use rspec expect syntax.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'spells'
 
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
